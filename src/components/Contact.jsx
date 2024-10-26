baa28a9b964d1a87d65f91d073546f90b0a8a7da
Moin khan <110406237+CodexSpy@users.noreply.github.com>
2024-10-26 15:51:34 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
   return (
     <div className=' border-b border-neutral-900 pb-20'>
         <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y: -100}} transition={{duration: 1.5 }} className='my-10 text-4xl text-center'>Get In Touch!</motion.h1>
-        <motion.div whileInView={{opacity:1, y:0}} initial={{opacity:0, y: 100}} transition={{duration: 1.5 }} className='text-center tracking-tighter'>
+        <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x: -100}} transition={{duration: 1.5 }} className='text-center tracking-tighter'>
         <p className='my-4 flex justify-center items-center'>
           {CONTACT.address}
           <span className='ml-2'>
