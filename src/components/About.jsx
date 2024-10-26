6a8478d919d1cbdf3db6def5a8d72564d2322971
Moin khan <110406237+CodexSpy@users.noreply.github.com>
2024-10-26 11:53:20 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import aboutImg from '../assets/AboutMe.webp'
-import { ABOUT_TEXT } from '../constants'
-import {motion} from 'framer-motion'
+import React from 'react';
+import aboutImg from '../assets/AboutMe.webp';
+import { ABOUT_TEXT } from '../constants';
+import { motion } from 'framer-motion';
 
-const About = () => {
+const About = ({ isDarkTheme }) => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h1 className='my-20 text-center  text-4xl'>About <span className='text-neutral-400'>Myself</span></h1>
-    <div className='flex flex-wrap'>
-        <motion.div whileInView={{opacity:1, x:0 }} initial={{opacity:0, x:-100}} transition={{duration:0.6}} className='w-full lg:w-1/2 lg:p-8'>
-            <div className='flex items-center justify-center'>
-                <img className='rounded-2xl  mr-6 ' src={aboutImg} alt="about" />
-            </div>
+      <h1 className={`my-20 text-center text-4xl ${isDarkTheme ? 'text-white' : 'text-black'}`}>
+        About <span className='text-neutral-400'>Myself</span>
+      </h1>
+      <div className='flex flex-wrap'>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.6 }}
+          className='w-full lg:w-1/2 lg:p-8'
+        >
+          <div className='flex items-center justify-center'>
+            <img className='rounded-2xl mr-6' src={aboutImg} alt="about" />
+          </div>
         </motion.div>
-     <motion.div whileInView={{opacity: 1, x:0}} initial={{opacity:0, x:100}} transition={{duration:0.6}} className='w-full lg:w-1/2'>
-<div className='flex justify-center lg:justify-start'>
-    <p className='my-2 max-w-xl py-6 px-4 font-light tracking-tight text-xl text-white bg-white/20 backdrop-blur-lg rounded-lg border border-white/30 shadow-lg' >{ABOUT_TEXT}</p>
-</div>
-            </motion.div>
-        </div>
-      
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.6 }}
+          className='w-full lg:w-1/2'
+        >
+          <div className='flex justify-center lg:justify-start'>
+            <p
+              className={`my-2 max-w-xl py-6 px-4 font-light tracking-tight text-xl ${isDarkTheme ? 'text-white' : 'text-black'} bg-white/20 backdrop-blur-lg rounded-lg border border-white/30 shadow-lg`}
+            >
+              {ABOUT_TEXT}
+            </p>
+          </div>
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
