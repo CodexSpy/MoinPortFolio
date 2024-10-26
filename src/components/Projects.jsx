5704360b247e83f390c74996b4cb671cec5d5449
Moin khan <110406237+CodexSpy@users.noreply.github.com>
2024-10-26 12:20:20 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,12 +15,12 @@ const Projects = () => {
                 </motion.div>
                 <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x: 100}} transition={{duration: 1.2 }} className='w-full max-w-xl lg:w-3/4'>
                 <h6 className='mb-2 font-semibold'>{project.title}</h6>
-                <p className='mb-4 text-neutral-400'>{project.description}</p>
+                <p className='mb-4 text-neutral-400 tracking-tight'>{project.description}</p>
                 {project.technologies.map((tech, index) =>(
                     <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-indigo-400'>{tech}</span>
                 ))}
                 </motion.div>
-                 <motion.a href={project.link} whileInView={{opacity:1, x:0}} initial={{opacity:0, x: 100}} transition={{duration: 1.2 }}  className='mr-2  text-indigo-500 text-2xl'><HiExternalLink /></motion.a>
+                 <motion.a href={project.link} whileInView={{opacity:1, x:0}} initial={{opacity:0, x: 100}} transition={{duration: 1.2 }}  className='mr-2 mt-2 text-indigo-500 text-2xl'><HiExternalLink /></motion.a>
                 </div>
             ))}
         </div>
