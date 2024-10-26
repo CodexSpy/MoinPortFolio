2a3f0de436063f3ea99ed3e244f540aa4979265e
Moin khan <110406237+CodexSpy@users.noreply.github.com>
2024-10-26 11:53:42 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import { EXPERIENCES } from '../constants'
-import {motion} from 'framer-motion'
+import React from 'react';
+import { EXPERIENCES } from '../constants';
+import { motion } from 'framer-motion';
 
-const Experience = () => {
+const Experience = ({ isDarkTheme }) => {
   return (
-    <div className=' border-b border-neutral-900 pb-4'>
-        <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y: -100}} transition={{duration: 1.5 }} className='my-20 text-center text-4xl'>Education</motion.h1>
-        <div>
-            {EXPERIENCES.map((experience, index) => (
-            <div key={index} className='mb-8 flex flex-wrap  text-left lg:justify-center '>
-                <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x: -100}} transition={{duration: 1.5 }} className='w-full lg:w-1/4'>
-                    <p className='mb-2 text-sm  text-neutral-400'>{experience.year}</p>
-                </motion.div>
-                <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x: 100}} transition={{duration: 1.5 }} className='w-full max-w-xl lg:w-3/4'>
-                <h6 className='mb-2 font-semibold'>
-                    {experience.cousre} - <span className='text-sm text-indigo-200'>{experience.College}</span>
-                </h6>
-                <p className='mb-4 '>{experience.description}</p>
-                {experience.Subjects.map((tech, index) => (
-                    <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-indigo-400'>{tech}</span>
-                ))}
-                
-                </motion.div>
-            </div>
-            ))}
-        </div>
-      
+    <div className='border-b border-neutral-900 pb-4'>
+      <motion.h1 
+        whileInView={{ opacity: 1, y: 0 }} 
+        initial={{ opacity: 0, y: -100 }} 
+        transition={{ duration: 1.5 }} 
+        className={`my-20 text-center text-4xl ${isDarkTheme ? 'text-white' : 'text-black'}`}
+      >
+        Education
+      </motion.h1>
+      <div>
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className='mb-8 flex flex-wrap text-left lg:justify-center'>
+            <motion.div 
+              whileInView={{ opacity: 1, x: 0 }} 
+              initial={{ opacity: 0, x: -100 }} 
+              transition={{ duration: 1.5 }} 
+              className='w-full lg:w-1/4'
+            >
+              <p className={`mb-2 text-sm ${isDarkTheme ? 'text-neutral-400' : 'text-neutral-600'}`}>
+                {experience.year}
+              </p>
+            </motion.div>
+            <motion.div 
+              whileInView={{ opacity: 1, x: 0 }} 
+              initial={{ opacity: 0, x: 100 }} 
+              transition={{ duration: 1.5 }} 
+              className='w-full max-w-xl lg:w-3/4'
+            >
+              <h6 className={`mb-2 font-semibold ${isDarkTheme ? 'text-white' : 'text-black'}`}>
+                {experience.cousre} - <span className={`text-sm ${isDarkTheme ? 'text-indigo-300' : 'text-indigo-800'}`}>{experience.College}</span>
+              </h6>
+              <p className={`mb-4 ${isDarkTheme ? 'text-gray-300' : 'text-black'}`}>
+                {experience.description}
+              </p>
+              {experience.Subjects.map((tech, index) => (
+                <span 
+                  key={index} 
+                  className={`mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium ${isDarkTheme ? 'text-indigo-400' : 'text-indigo-400'}`}
+                >
+                  {tech}
+                </span>
+              ))}
+            </motion.div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
