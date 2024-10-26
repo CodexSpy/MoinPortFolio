958e8ed757f76d3b97ee5200da7905ddc3230b7f
Moin khan <110406237+CodexSpy@users.noreply.github.com>
2024-10-26 11:51:28 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        DEFAULT: '2px 2px 4px rgba(0, 0, 0, 0.5)', // Default text shadow
+        light: '1px 1px 2px rgba(255, 255, 255, 0.7)', // Light text shadow
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const textShadowUtilities = {
+        '.text-shadow': {
+          'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-light': {
+          'text-shadow': '1px 1px 2px rgba(255, 255, 255, 0.7)',
+        },
+        '.text-shadow-yellow': {
+          'text-shadow': '2px 2px 4px rgba(255, 255, 0, 0.7)', // Yellow shadow utility
+        },
+      };
+      addUtilities(textShadowUtilities, ['responsive', 'hover']);
+    },
+  ],
 }
